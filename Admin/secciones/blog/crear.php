e8fd1371d83681e972b385cedb3ee03deaef47e3
ALEJANDRO PINTOS. <148916132+NOVATCODER@users.noreply.github.com>
2024-06-25 19:35:45 -0300

    actualizacion

--- Admin/secciones/blog/crear.php
+++ Admin/secciones/blog/crear.php
@@ -1,7 +1,7 @@
 <?php 
-include("../../db.php");
+include("../../db.php");//inclusion de la base de datos.
 
-if($_POST){
+if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     //Recepcionamos los valores del formulario.
     $titulo = (isset($_POST['titulo'])) ? $_POST['titulo']  :   "";
@@ -9,17 +9,18 @@ if($_POST){
     $imagen = (isset($_FILES["imagen"]["name"])) ?  $_FILES["imagen"]   :   "";
 
     $fecha_imagen=new   DateTime();
-    $nombre_archivo_imagen=($imagen !="")?$fecha_imagen->getTimestamp()."_".$imagen:"";
+    $nombre_archivo_imagen=($imagen !="")?$fecha_imagen->getTimestamp()."_".$imagen: "";
 
     $tmp_imagen = $_FILES["imagen"]["tmp_name"];
     if($tmp_imagen!=""){
         move_uploaded_file($tmp_imagen, "../../../assets/img/blog/").$nombre_archivo_imagen;
     }
 //Insercion de los datos en la base.
-$sentencia = $conexion->prepare("INSERT INTO `tabla_blog` (`ID`, `titulo`,`descripcion` ,`imagen`) 
+$sentencia = $conexion->prepare("INSERT INTO `tabla_blog` (`titulo`,`descripcion` ,`imagen`) 
     VALUES (NULL, :titulo,  :descripcion, :imagen)");
 
     $sentencia->bindParam(":titulo", $titulo);
+    $sentencia->bindParam(":descripcion", $descripcion);
     $sentencia->bindParam(":imagen", $nombre_archivo_imagen);
 
     $sentencia->execute();
@@ -52,7 +53,7 @@ include("../../templates/header.php");
         <div class="mb-3">
             <label for="" class="form-label">Imágen:</label>
             <input
-                type="text"
+                type="file"
                 class="form-control"
                 name="imagen"
                 id="imagen"
@@ -61,7 +62,7 @@ include("../../templates/header.php");
             />
         </div>
         <div class="mb-3">
-            <label for="" class="form-label">Descripcion:</label>
+            <label for="" class="form-label">Descripción:</label>
             <input
                 type="text"
                 class="form-control"
@@ -71,7 +72,8 @@ include("../../templates/header.php");
                 placeholder="Descripcion"
             />
         </div>
-        
+        <button type="submit" class="btn btn-success">Agregar</button>
+        <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
     </div>
     <div class="card-footer text-muted"></div>
 
