af80b1339daeec7a85fd02ddd6182f00ee73282f
ALEJANDRO PINTOS. <148916132+NOVATCODER@users.noreply.github.com>
2024-06-26 15:38:25 -0300

    Actualizacion, crud del blog completamente funcional

--- Admin/secciones/blog/crear.php
+++ Admin/secciones/blog/crear.php
@@ -4,29 +4,30 @@ include("../../db.php");//inclusion de la base de datos.
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     //Recepcionamos los valores del formulario.
+    $imagen	 = (isset($_FILES["imagen"]["name"])) ?  $_FILES["imagen"]["name"]   :   "";
     $titulo = (isset($_POST['titulo'])) ? $_POST['titulo']  :   "";
     $descripcion = (isset($_POST['descripcion'])) ? $_POST['descripcion']  :   "";
-    $imagen = (isset($_FILES["imagen"]["name"])) ?  $_FILES["imagen"]   :   "";
+
 
     $fecha_imagen=new   DateTime();
-    $nombre_archivo_imagen=($imagen !="")?$fecha_imagen->getTimestamp()."_".$imagen: "";
+    $nombre_archivo_imagen=($imagen != "")? $fecha_imagen->getTimestamp() ."_". $imagen: "";
 
     $tmp_imagen = $_FILES["imagen"]["tmp_name"];
     if($tmp_imagen!=""){
-        move_uploaded_file($tmp_imagen, "../../../assets/img/blog/").$nombre_archivo_imagen;
+        move_uploaded_file($tmp_imagen, "../../../assets/img/blog/" . $nombre_archivo_imagen);
     }
 //Insercion de los datos en la base.
-$sentencia = $conexion->prepare("INSERT INTO `tabla_blog` (`titulo`,`descripcion` ,`imagen`) 
-    VALUES (NULL, :titulo,  :descripcion, :imagen)");
+$sentencia = $conexion->prepare("INSERT INTO `tbl_blog`(`imagen`,`titulo` ,`descripcion`) 
+    VALUES (:imagen,  :titulo, :descripcion)");
 
+    $sentencia->bindParam(":imagen", $nombre_archivo_imagen);
     $sentencia->bindParam(":titulo", $titulo);
     $sentencia->bindParam(":descripcion", $descripcion);
-    $sentencia->bindParam(":imagen", $nombre_archivo_imagen);
 
     $sentencia->execute();
 
     $mensaje="Registro agregado con éxito.";
-    header("Location:index.php?mensjae=".$mensaje);
+    header("Location:index.php?mensaje=".$mensaje);
     exit();
 }
 include("../../templates/header.php");
@@ -40,17 +41,6 @@ include("../../templates/header.php");
         <form action="" enctype="multipart/form-data" method="post">
     
         <div class="mb-3">
-            <label for="" class="form-label">Título:</label>
-            <input
-                type="text"
-                class="form-control"
-                name="titulo"
-                id="titulo"
-                aria-describedby="helpId"
-                placeholder="Título"
-            />
-        </div>
-        <div class="mb-3">
             <label for="" class="form-label">Imágen:</label>
             <input
                 type="file"
@@ -62,6 +52,17 @@ include("../../templates/header.php");
             />
         </div>
         <div class="mb-3">
+            <label for="" class="form-label">Título:</label>
+            <input
+                type="text"
+                class="form-control"
+                name="titulo"
+                id="titulo"
+                aria-describedby="helpId"
+                placeholder="Título"
+            />
+        </div>
+        <div class="mb-3">
             <label for="" class="form-label">Descripción:</label>
             <input
                 type="text"
@@ -69,7 +70,7 @@ include("../../templates/header.php");
                 name="descripcion"
                 id="descripcion"
                 aria-describedby="helpId"
-                placeholder="Descripcion"
+                placeholder="Descripción"
             />
         </div>
         <button type="submit" class="btn btn-success">Agregar</button>
