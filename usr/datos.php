3276d16742eafc39d6a8d472b1937ed1652a49f1
Nery <vnd22victor@gmail.com>
2024-06-26 15:39:43 -0300

    form datos

--- usr/datos.php
+++ usr/datos.php
@@ -7,7 +7,7 @@ include("template/header.php");
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800 border-left-primary border-bottom-primary">CPA</h1>
+                        <h1 class="h3 mb-0 text-gray-800 border-left-primary border-bottom-primary">Club Pabellón Argentino</h1>
                         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                 class="fas fa-download fa-sm text-white-50"></i> Generar carnet</a>
                     </div>
@@ -26,40 +26,72 @@ include("template/header.php");
                         <h1 class="h4 text-gray-900 mb-4">Modificar datos</h1>
                     </div>
                     <form class="user">
+                        <div class="text-center">
+                            <label for="" class="form-label"></label>
+                            <img class="img-fluid rounded-sm rounded-circle " width="75" height="75" src="../assets/images/person-1.jpg" alt="profile">
+                            <hr>
+                            
+                            <input
+                                type="file"
+                                class="form-control"
+                                name=""
+                                id=""
+                                placeholder=""
+                                aria-describedby="fileHelpId"
+                            />
+                            <div id="fileHelpId" class="form-text">Completar campos</div>
+                        </div>
+                        
                         <div class="form-group row">
                             <div class="col-sm-6 mb-3 mb-sm-0">
                                 <input type="text" class="form-control form-control-user" id="exampleFirstName"
-                                    placeholder="First Name">
+                                    placeholder="Nombre">
                             </div>
                             <div class="col-sm-6">
                                 <input type="text" class="form-control form-control-user" id="exampleLastName"
-                                    placeholder="Last Name">
+                                    placeholder="Apellido">
                             </div>
                         </div>
+                        <div id="fileHelpId" class="form-text">-Para cambiar nombre y apellido debe contactar al administrador</div>
+                        <hr>
                         <div class="form-group">
                             <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                                placeholder="Email Address">
+                                placeholder="Email">
                         </div>
                         <div class="form-group row">
                             <div class="col-sm-6 mb-3 mb-sm-0">
-                                <input type="password" class="form-control form-control-user"
-                                    id="exampleInputPassword" placeholder="Password">
+                                <input type="number" class="form-control form-control-user"
+                                    id="" placeholder="Telefono/Celular">
                             </div>
                             <div class="col-sm-6">
-                                <input type="password" class="form-control form-control-user"
-                                    id="exampleRepeatPassword" placeholder="Repeat Password">
+                                <input type="text" class="form-control form-control-user"
+                                    id="exampleRepeatPassword" placeholder="Calle">
                             </div>
+                            <div class="mb-3">
+                                <label for="" class="form-label"></label>
+                                <select
+                                    class="form-select form-select-lg"
+                                    name=""
+                                    id=""
+                                >
+                                    <option selected>Ciudad</option>
+                                    <option value="">El Colorado</option>
+                                    <option value="">Villa 213</option>
+                                    <option value="">Villafañe</option>
+                                    <option value="">Pirané</option>
+                                    <option value="">Formosa</option>
+                                </select>
+                            </div>
+                            
                         </div>
                         <a href="login.html" class="btn btn-primary btn-user btn-block">
                             Guardar
                         </a>
+                        <a href="index.php" class="btn btn-danger btn-user btn-block">
+                            Cancelar
+                        </a>
                         <hr>
-                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                            <i class="fab fa-google fa-fw"></i> 
-                        </a>
-                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                            <i class="fab fa-facebook-f fa-fw"></i> 
-                        </a>
+                        
                     </form>
                     <hr>
                     <div class="text-center">
--- usr/index.php
+++ usr/index.php
@@ -6,7 +6,7 @@ include("template/header.php");
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800 border-left-primary border-bottom-primary">CPA</h1>
+                        <h1 class="h3 mb-0 text-gray-800 border-left-primary border-bottom-primary">Club Pabellón Argentino</h1>
                         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                 class="fas fa-download fa-sm text-white-50"></i> Generar carnet</a>
                     </div>
